58f76214167d797fe6aad69844aec4162f9c1d01
Rico <rico1310@live.com>
2018-05-20 20:53:51 +0200

    End of Workshop2: Unity

---
+++ 3d/Assets/Artillery Tank/unity_script1.js
@@ -0,0 +1,8 @@
+#pragma strict
+var flame : GameObject; function Start () {
+flame.SetActive(false);
+}
+function OnTriggerEnter(){ flame.SetActive(true);
+}
+function OnTriggerExit(){ flame.SetActive(false);
+} 
